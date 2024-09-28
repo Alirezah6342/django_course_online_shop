d4173be18af7c0dc13a9829d02c45b2b9bd8f02e
Alireza Hassani <alirezahasani563@gmail.com>
2024-09-28 21:46:25 +0330

    add pages app with HomePageView and AboutUsPageView and SignUpView

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+from . forms import CustomUserCreationForm
+
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+
